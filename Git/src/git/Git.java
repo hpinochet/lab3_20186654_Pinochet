c055a33b9c138e4eba8d97516371d9f32815b7be
Hernan Pinochet <hernan.pinochet@usach.cl>
2020-08-26 19:27:44 -0400

    Se agrega gitInit

--- Git/src/git/Git.java
+++ Git/src/git/Git.java
@@ -2,4 +2,9 @@ package git;
 
 public class Git {
     
+    public static ZonaTrabajo gitinit(String nombreRepositorio, String nombreAutor){
+        ZonaTrabajo nuevaZona = new ZonaTrabajo(nombreRepositorio,nombreAutor);
+        return nuevaZona;
+    }
+    
 }
