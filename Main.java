bd02370e706cc7d541097957c98a81ba24bd5da3
Hernan Pinochet <hernan.pinochet@usach.cl>
2020-09-07 00:08:42 -0400

    Programa Version Final: Se arregla partes procedurales a orientada a objetos y se agregan comentarios tipo Javadoc

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
-package git;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+/**
+ * Clase que representa la interfaz de usuario y realiza los llamados de la funcion git
+ * @author Hernan
+ */
+public class Main{
 
     public static void main(String[] args) {
         
-        System.out.println("### BIENVENIDO A SIMULACIÓN GIT ###");
+        // Inicio del programa
+        
+        System.out.println("\n### BIENVENIDO A SIMULACION GIT ###");
         System.out.println("### Cree su nuevo repositorio ###");
         
         Scanner objRepo = new Scanner(System.in);
@@ -16,17 +21,22 @@ public class Main {
         Scanner objAutor = new Scanner(System.in);
         System.out.println("Ingrese nombre autor de repositorio: ");
         String nombreAutor = objAutor.nextLine();
-                
-        ZonaTrabajo Zona = Git.gitinit(nombreRepo, nombreAutor);
+        
+        // Instanciar la clase con los parametors reflejaria nuestra funcion gitInit
+        
+        ZonaTrabajo Zona = new ZonaTrabajo(nombreRepo,nombreAutor);
+        
         System.out.println(" ");
         System.out.println(Zona);
         System.out.println(" ");
         
         int opcion = 0;
         
+        // Menu
+        
         while(opcion != 8){
-            System.out.println("### SIMULACIÓN GIT ###");
-            System.out.println("Escoja su opción");
+            System.out.println("### SIMULACION GIT ###");
+            System.out.println("Escoja su opcion");
             System.out.println("1. Crear nuevo archivo");
             System.out.println("2. add");
             System.out.println("3. commit");
@@ -36,38 +46,56 @@ public class Main {
             System.out.println("7. log");
             System.out.println("8. salir");
             Scanner objOpcion = new Scanner(System.in);
-            System.out.println("INTRODUZCA SU OPCIÓN: ");
-            opcion = objOpcion.nextInt();
+            System.out.println("INTRODUZCA SU OPCION: ");
+            
+            // Esto es en caso de que haya error al ingresar una opcion,
+            //el try pasara la opcion sin modificacion por lo cual entraria a una opcion anterior
+            opcion = 9;
+            
+            try{
+                opcion = objOpcion.nextInt();
+                
+                if(opcion < 1 || opcion > 9){
+                    System.out.println(" \n! Error, introduzca una opcion valida.\n");
+                }
+            }catch(InputMismatchException e){
+               System.out.println("\n! Error, introduzca una opcion valida.\n");
+               objOpcion.next();
+            }
             
             System.out.println(" ");
             
+            // Opciones Git
+            
             if(opcion == 1){
-                Zona = Git.gitCrear(Zona);
+                Zona.gitCrear();
             }
             
             if(opcion == 2){
-                Zona = Git.gitAdd(Zona);
+                Zona.gitAdd();
             }
+            
             
             if(opcion == 3){
-                Zona = Git.gitCommit(Zona);
+                Zona.gitCommit();
             }
             
+            
             if(opcion == 4){
-                Zona = Git.gitPull(Zona);
+                Zona.gitPull();
             }
-  
+            
             if(opcion == 5){
-                Zona = Git.gitPush(Zona);
+                Zona.gitPush();
             }
-       
+            
             if(opcion == 6){
-                Git.gitStatus(Zona);
+                Zona.gitStatus();
                 System.out.println(" ");
             }
             
             if(opcion == 7){
-                Git.gitLog(Zona);
+                Zona.gitLog();
                 System.out.println(" ");
             }
             
